d5affaf0a591c51b46b02f304b9b8da089b07c0b
alexis.ehret <alexis.ehret@epita.fr>
2019-02-11 22:35:30 +0100

    refactor

--- src/emulator.c
+++ src/emulator.c
@@ -36,339 +36,385 @@ int shutdown() {
  * @return -1 => error || other => OK
  */
 int next_instruction() {
-    uint16_t current_operation;
+    uint16_t current_operation = read_16bit(memory + PC);
 
-    current_operation = read_16bit(memory + PC);
+    // mask
+    uint16_t mask_0xf000 = 0xf000 & current_operation;
+    uint16_t mask_0xf018 = 0xf018 & current_operation;
+    uint16_t mask_0xf0c0 = 0xf0c0 & current_operation;
+    uint16_t mask_0xf100 = 0xf100 & current_operation;
+    uint16_t mask_0xf130 = 0xf130 & current_operation;
+    uint16_t mask_0xf138 = 0xf138 & current_operation;
+    uint16_t mask_0xf1c0 = 0xf1c0 & current_operation;
+    uint16_t mask_0xf1f0 = 0xf1f0 & current_operation;
+    uint16_t mask_0xfec0 = 0xfec0 & current_operation;
+    uint16_t mask_0xff00 = 0xff00 & current_operation;
+    uint16_t mask_0xffc0 = 0xffc0 & current_operation;
+    uint16_t mask_0xfff0 = 0xfff0 & current_operation;
+    uint16_t mask_0xfff8 = 0xfff8 & current_operation;
     
-    warnx("Warning instruction not implemented => %hx", current_operation);
-    return -1;
-}
-
-inline int is_ori_to_ccr(uint16_t current_operation) {
-    return current_operation == 0x003c;
-}
-
-inline int is_ori_to_sr(uint16_t current_operation) {
-    return current_operation == 0x007c;
-}
-
-inline int is_ori(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x0000;
-}
-
-inline int is_andi_to_ccr(uint16_t current_operation) {
-    return current_operation == 0x023c;
-}
-
-inline int is_andi_to_sr(uint16_t current_operation) {
-    return current_operation == 0x27c;
-}
-
-inline int is_andi(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x0200;
-}
-
-inline int is_subi(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x0400;
-}
-
-inline int is_addi(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x0600;
-}
-
-inline int is_eori_to_ccr(uint16_t current_operation) {
-    return current_operation == 0x0a3c;
-}
-
-inline int is_eori_to_sr(uint16_t current_operation) {
-    return current_operation == 0x0a7c;
-}
-
-inline int is_eori(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x0a00;
-}
-
-inline int is_cmpi(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x0c00;
-}
-
-inline int is_btst(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x0800
-        || (0xf1c0 & current_operation) == 0x0100;
-}
-
-inline int is_bchg(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x0840
-        || (0xf1c0 & current_operation) == 0x0140;
-}
-
-inline int is_bclr(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x0880
-        || (0xf1c0 & current_operation) == 0x0180;
-}
-
-inline int is_bset(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x08c0
-        || (0xf1c0 & current_operation) == 0x01c0;
-}
-
-inline int is_movep(uint16_t current_operation) {
-    return (0xf138 & current_operation) == 0x0108;
-}
-
-inline int is_movea(uint16_t current_operation) {
-    return (0xc1c0 & current_operation) == 0x0040;
-}
-
-inline int is_move(uint16_t current_operation) {
-    return (0xc000 & current_operation) == 0x0000;
-}
-
-inline int is_move_from_sr(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x40c0;
-}
-
-inline int is_move_to_ccr(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x44c0;
-}
-
-inline int is_move_to_sr(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x46c0;
-}
-
-inline int is_negx(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x4000;
-}
-
-inline int is_clr(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x4200;
-}
-
-inline int is_neg(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x4400;
-}
-
-inline int is_not(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x4600;
-}
-
-inline int is_ext(uint16_t current_operation) {
-    return (0xffb8 & current_operation) == 0x4880;
-}
-
-inline int is_nbcd(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x4800;
-}
-
-inline int is_swap(uint16_t current_operation) {
-    return (0xfff8 & current_operation) == 0x4840;
-}
-
-inline int is_pea(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x4840;
-}
-
-inline int is_illegal(uint16_t current_operation) {
-    return current_operation == 0x4afc;
-}
-
-inline int is_tas(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x4ac0;
-}
-
-inline int is_tst(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x4a00;
-}
-
-inline int is_trap(uint16_t current_operation) {
-    return (0xfff0 & current_operation) == 0x4e40;
-}
-
-inline int is_link(uint16_t current_operation) {
-    return (0xfff8 & current_operation) == 0x4e50;
-}
-
-inline int is_unlk(uint16_t current_operation) {
-    return (0xfff8 & current_operation) == 0x4e58;
-}
-
-inline int is_move_usp(uint16_t current_operation) {
-    return (0xfff0 & current_operation) == 0x4e60;
-}
-
-inline int is_reset(uint16_t current_operation) {
-    return current_operation == 0x4e70;
-}
-
-inline int is_nop(uint16_t current_operation) {
-    return current_operation == 0x4e71;
-}
-
-inline int is_stop(uint16_t current_operation) {
-    return current_operation == 0x4e72;
-}
-
-inline int is_rte(uint16_t current_operation) {
-    return current_operation == 0x4e73;
-}
-
-inline int is_rts(uint16_t current_operation) {
-    return current_operation == 0x4e75;
-}
-
-inline int is_trapv(uint16_t current_operation) {
-    return current_operation == 0x4e76;
-}
-
-inline int is_rtr(uint16_t current_operation) {
-    return current_operation == 0x4e77;
-}
-
-inline int is_jsr(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x4e80;
-}
-
-inline int is_jmp(uint16_t current_operation) {
-    return (0xffc0 & current_operation) == 0x4ec0;
-}
-
-inline int is_movem(uint16_t current_operation) {
-    return (0xfb80 & current_operation) == 0x4880;
-}
-
-inline int is_lea(uint16_t current_operation) {
-    return (0xf1c0 & current_operation) == 0x41c0;
-}
-
-inline int is_chk(uint16_t current_operation) {
-    return (0xf1c0 & current_operation) == 0x4180;
-}
-
-inline int is_addq(uint16_t current_operation) {
-    return (0xf100 & current_operation) == 0x5000;
-}
-
-
-inline int is_subq(uint16_t current_operation) {
-    return (0xf100 & current_operation) == 0x5100;
-}
-
-inline int is_dbcc(uint16_t current_operation) {
-    return (0xf0f8 & current_operation) == 0x50c8;
-}
-
-inline int is_scc(uint16_t current_operation) {
-    return (0xf0c0 & current_operation) == 0x50c0;
-}
-
-inline int is_bra(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x6000;
-}
-
-inline int is_bsr(uint16_t current_operation) {
-    return (0xff00 & current_operation) == 0x6100;
-}
-
-inline int is_bcc(uint16_t current_operation) {
-    return (0xf000 & current_operation) == 0x6000;
-}
-
-inline int is_moveq(uint16_t current_operation) {
-    return (0xf100 & current_operation) == 0x7000;
-}
-
-inline int is_divu(uint16_t current_operation) {
-    return (0xf1c0 & current_operation) == 0x80c0;
-}
-
-inline int is_divs(uint16_t current_operation) {
-    return (0xf1c0 & current_operation) == 0x81c0;
-}
-
-inline int is_sbcd(uint16_t current_operation) {
-    return (0xf1f0 & current_operation) == 0x8100;
-}
-
-inline int is_or(uint16_t current_operation) {
-    return (0xf000 & current_operation) == 0x8000;
-}
-
-inline int is_subx(uint16_t current_operation) {
-    return (0xf130 & current_operation) == 0x9100;
-}
-
-inline int is_suba(uint16_t current_operation) {
-    return (0xf0c0 & current_operation) == 0x90c0;
-}
-
-inline int is_sub(uint16_t current_operation) {
-    return (0xf000 & current_operation) == 0x9000;
-}
-
-inline int is_cmpm(uint16_t current_operation) {
-    return (0xf138 & current_operation) == 0xb108;
-}
-
-inline int is_eor(uint16_t current_operation) {
-    return (0xf100 & current_operation) == 0xb100;
-}
-
-inline int is_cmp(uint16_t current_operation) {
-    return (0xf100 & current_operation) == 0xb000;
-}
-
-inline int is_cmpa(uint16_t current_operation) {
-    return (0xf0c0 & current_operation) == 0xb0c0;
-}
-
-inline int is_mulu(uint16_t current_operation) {
-    return (0xf1c0 & current_operation) == 0xc0c0;
-}
-
-inline int is_muls(uint16_t current_operation) {
-    return (0xf1c0 & current_operation) == 0xc1c0;
-}
-
-inline int is_abcd(uint16_t current_operation) {
-    return (0xf1f0 & current_operation) == 0xc100;
-}
-
-inline int is_exg(uint16_t current_operation) {
-    return (0xf130 & current_operation) == 0xc100;
-}
-
-inline int is_and(uint16_t current_operation) {
-    return (0xf000 & current_operation) == 0xc000;
-}
-
-inline int is_addx(uint16_t current_operation) {
-    return (0xf130 & current_operation) == 0xd100;
-}
-
-inline int is_adda(uint16_t current_operation) {
-    return (0xf0c0 & current_operation) == 0xd0c0;
-}
-
-inline int is_add(uint16_t current_operation) {
-    return (0xf000 & current_operation) == 0xd000;
-}
-
-inline int is_asd(uint16_t current_operation) {
-    return (0xfec0 & current_operation) == 0xe0c0
-        || (0xf018 & current_operation) == 0xe000;
-}
-
-inline int is_lsd(uint16_t current_operation) {
-    return (0xfec0 & current_operation) == 0xe2c0
-        || (0xf018 & current_operation) == 0xe008;
-}
-
-inline int is_roxd(uint16_t current_operation) {
-    return (0xfec0 & current_operation) == 0xe4c0
-        || (0xf018 & current_operation) == 0xe010;
-}
-
-inline int is_rod(uint16_t current_operation) {
-    return (0xfec0 & current_operation) == 0xe6c0
-        || (0xf018 & current_operation) == 0xe018;
+    // hardcoded function
+    switch (current_operation) {
+        case 0x003c:
+            // is_ori_to_ccr
+            goto warning;
+        case 0x007c:
+            // is_ori_to_sr
+            goto warning;
+        case 0x023c:
+            // is_andi_to_ccr
+            goto warning;
+        case 0x27c:
+            // is_andi_to_sr
+            goto warning;
+        case 0x0a3c:
+            // is_eori_to_ccr
+            goto warning;
+        case 0x0a7c:
+            // is_eori_to_sr
+            goto warning;
+        case 0x4afc:
+            // is_illegal
+            goto warning;
+        case 0x4e70:
+            // is_reset
+            goto warning;
+        case 0x4e71:
+            // is_nop
+            goto warning;
+        case 0x4e72:
+            // is_stop
+            goto warning;
+        case 0x4e73:
+            // is_rte
+            goto warning;
+        case 0x4e75:
+            // is_rts
+            goto warning;
+        case 0x4e76:
+            // is_trapv
+            goto warning;
+        case 0x4e77:
+            // is_rtr
+            goto warning;
+    }
+
+    switch(mask_0xff00) {
+        case 0x0000:
+            // is_ori
+            goto warning;
+        case 0x0200:
+            // is_andi
+            goto warning;
+        case 0x0400:
+            // is_subi
+            goto warning;
+        case 0x0600:
+            // is_addi
+            goto warning;
+        case 0x0a00:
+            // is_eori
+            goto warning;
+        case 0x0c00:
+            // is_cmpi
+            goto warning;
+    }
+
+    if (mask_0xffc0 == 0x0800 || mask_0xf1c0 == 0x0100) {
+        // is_btst
+        goto warning;
+    }
+
+    if (mask_0xffc0 == 0x0840 || mask_0xf1c0 == 0x0140) {
+        // is_bchg
+        goto warning;
+    }
+
+    if (mask_0xffc0 == 0x0880 || mask_0xf1c0 == 0x0180) {
+        // is_bclr
+        goto warning;
+    }
+
+    if (mask_0xffc0 == 0x08c0 || mask_0xf1c0 == 0x01c0) {
+        // is_bset
+        goto warning;
+    }
+
+    if (mask_0xf138 == 0x0108) {
+        // is_movep
+    }
+
+    if ((0xc1c0 & current_operation) == 0x0040) {
+        // is_movea
+    }
+    
+    if ((0xc000 & current_operation) == 0x0000) {
+        // is_move
+    }
+
+    switch (mask_0xf1c0) {
+        case 0x40c0:
+            // is_move_from_sr
+            goto warning;
+        case 0x44c0:
+            // is_move_to_ccr
+            goto warning;
+        case 0x46c0:
+            // is_move_to_sr
+            goto warning;
+    }
+
+    switch (mask_0xff00) {
+        case 0x4000:
+            // is_negx
+            goto warning;
+        case 0x4200:
+            // is_clr
+            goto warning;
+        case 0x4400:
+            // is_neg
+            goto warning;
+        case 0x4600:
+            // is_not
+            goto warning;
+    }
+
+    if ((0xffb8 & current_operation) == 0x4880) {
+        // is_ext
+        goto warning;
+    }
+
+    if (mask_0xffc0 == 0x4800) {
+        // is_nbcd
+        goto warning;
+    }
+
+    if ((0xfff8 & current_operation) == 0x4840) {
+        // is_swap
+        goto warning;
+    }
+
+    switch (mask_0xffc0) {
+        case 0x4840:
+            // is_pea
+            goto warning;
+        case 0x4ac0:
+            // is_tas
+            goto warning;
+    }
+
+    if (mask_0xff00 == 0x4a00) {
+        // is_tst
+        goto warning;
+    }
+    
+    if (mask_0xfff0 == 0x4e40) {
+        // is_is_trap
+        goto warning;
+    }
+
+    if (mask_0xfff8 == 0x4e50) {
+        // is_link
+        goto warning;
+    }
+
+    if (mask_0xfff8 == 0x4e58) {
+        // is_unlk
+        goto warning;
+    }
+
+    if (mask_0xfff0 == 0x4e60) {
+        // is_move_usp
+        goto warning;
+    }
+
+    if (mask_0xffc0 == 0x4e80) {
+        // is_jsr
+        goto warning;
+    }
+
+    if (mask_0xffc0 == 0x4ec0) {
+        // is_jmp
+        goto warning;
+    }
+
+    if ((0xfb80 & current_operation) == 0x4880) {
+        // is_movem
+        goto warning;
+    }
+
+    if (mask_0xf1c0 == 0x41c0) {
+        // is_lea
+        goto warning;
+    }
+
+    if (mask_0xf1c0 == 0x4180) {
+        // is_chk
+        goto warning;
+    }
+    
+    if (mask_0xf100 == 0x5000) {
+        // is_addq
+        goto warning;
+    }
+
+    if (mask_0xf100 == 0x5100) {
+        // is_subq
+        goto warning;
+    }
+
+    if ((0xf0f8 & current_operation) == 0x50c8) {
+        // is_dbcc
+        goto warning;
+    }
+
+    if (mask_0xf0c0 == 0x50c0) {
+        // is_scc
+        goto warning;
+    }
+
+    if (mask_0xff00 == 0x6000) {
+        // is_bra
+        goto warning;
+    }
+
+    if (mask_0xff00 == 0x6100) {
+        // is_bsr
+        goto warning;
+    }
+
+    if (mask_0xf000 == 0x6000) {
+        // is_bcc
+        goto warning;
+    }
+
+    if (mask_0xf100 == 0x7000) {
+        // is_moveq
+        goto warning;
+    }
+
+    if (mask_0xf1c0 == 0x80c0) {
+        // is_divu
+        goto warning;
+    }
+
+    if (mask_0xf1c0 == 0x81c0) {
+        // is_divs
+        goto warning;
+    }
+
+    if (mask_0xf1f0 == 0x8100) {
+        // is_sbcd
+        goto warning;
+    }
+
+    if (mask_0xf000 == 0x8000) {
+        // is_or
+        goto warning;
+    }
+
+    if (mask_0xf130 == 0x9100) {
+        // is_subx
+        goto warning;
+    }
+
+    if (mask_0xf0c0 == 0x90c0) {
+        // is_suba
+        goto warning;
+    }
+
+    if (mask_0xf000 == 0x9000) {
+        // is_sub 
+        goto warning;
+    }
+
+    if (mask_0xf138 == 0xb108) {
+        // is_cmpm
+        goto warning;
+    }
+
+    if (mask_0xf100 == 0xb100) {
+        // is_eor
+        goto warning;
+    }
+
+    if (mask_0xf100 == 0xb000) {
+        // is_cmp
+        goto warning;
+    }
+
+    if (mask_0xf0c0 == 0xb0c0) {
+        // is_cmpa
+        goto warning;
+    }
+    
+    if (mask_0xf1c0 == 0xc0c0) {
+        // is_mulu
+        goto warning;
+    }
+
+    if (mask_0xf1c0 == 0xc1c0) {
+        // is_muls
+        goto warning;
+    }
+    
+    if (mask_0xf1f0 == 0xc100) {
+        // is_abcd
+        goto warning;
+    }
+
+    if (mask_0xf130 == 0xc100) {
+        // is_exg
+        goto warning;
+    }
+
+    if (mask_0xf130 == 0xd100) {
+        // is_addx
+        goto warning;
+    }
+
+    if (mask_0xf0c0 == 0xd0c0) {
+        // is_adda
+        goto warning;
+    }
+
+    if (mask_0xf000 == 0xc000) {
+        // is_and
+        goto warning;
+    }
+
+    if (mask_0xf000 == 0xd000) {
+        // is_add
+        goto warning;
+    }
+
+    if (mask_0xfec0 == 0xe0c0 || mask_0xf018== 0xe000) {
+        // is_asd
+        goto warning;
+    }
+
+    if (mask_0xfec0 == 0xe2c0 || mask_0xf018== 0xe008) {
+        // is_lsd
+        goto warning;
+    }
+
+    if (mask_0xfec0 == 0xe4c0 || mask_0xf018== 0xe010) {
+        // is_roxd
+        goto warning;
+    }
+
+    if (mask_0xfec0 == 0xe6c0 || mask_0xf018== 0xe018) {
+        // is_rod
+        goto warning;
+    }
+
+    goto error;
+
+    warning:
+        warnx("Warning instruction not implemented => %hx", current_operation);
+        return -1;
+
+    error:
+        warnx("Unknown instruction not implemented => %hx", current_operation);
+        return -1;
 }
--- test/_t_emulator.c
+++ test/_t_emulator.c
@@ -4,15 +4,3 @@
 #include <criterion/criterion.h>
 
 #include "../src/emulator.h"
-
-Test(emulator, is_ori) {
-    for (uint16_t i = 0; i < 0xff; ++i) {
-        cr_assert(is_ori(i));
-    }
-}
-
-Test(emulator, is_andi) {
-    for (uint16_t i = 0; i < 0xff; ++i) {
-        cr_assert(is_andi(i + 0x200));
-    }
-}
